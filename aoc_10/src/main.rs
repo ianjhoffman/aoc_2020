b2da9cd7ceb602ca1d4c2161d9ba46f441685372
Ian Hoffman <ijh6@cornell.edu>
2020-12-09 21:52:00 -0800

    Day 10

---
+++ aoc_10/src/main.rs
@@ -0,0 +1,71 @@
+use std::path::PathBuf;
+use util::res::Result;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Cli {
+    #[structopt(short = "f", parse(from_os_str))]
+    file: PathBuf,
+}
+
+struct JoltageRating {
+    rating: u64,
+}
+
+impl std::str::FromStr for JoltageRating {
+    type Err = util::file::GenericParseError;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        Ok(JoltageRating{rating: s.parse::<u64>()?})
+    }
+}
+
+fn part1(joltages: &Vec<u64>) {
+    let (mut diffs_1, mut diffs_3, mut prev): (u64, u64, u64) = (0, 0, 0);
+    for joltage in joltages {
+        match *joltage - prev {
+            1 => diffs_1 += 1,
+            3 => diffs_3 += 1,
+            _ => (),
+        }
+        prev = *joltage;
+    }
+
+    diffs_3 += 1; // Final 3-joltage difference
+    println!("[Part 1] 1-Jolt differences: {}, 3-Jolt differences: {}, Product: {}", diffs_1, diffs_3, diffs_1 * diffs_3);
+}
+
+fn part2(joltages: &Vec<u64>) {
+    let mut arrangements_from_each: Vec<u64> = vec![0; joltages.len()];
+
+    // The highest-rated adapter can only do one arrangement - plugging into your device
+    arrangements_from_each[joltages.len() - 1] = 1;
+
+    for idx in (0..joltages.len() - 1).rev() {
+        // Get indices of accessible subsequent adapters with joltage rating difference <= 3
+        let accessible_indices: Vec<usize> = joltages.iter().enumerate().skip(idx + 1)
+            .take_while(|(_, &j)| j - joltages[idx] <= 3).map(|(later, _)| later).collect();
+        
+        // Add up all arrangements from accessible subsequent adapters to the end
+        arrangements_from_each[idx] = accessible_indices.iter().fold(0, |acc, &later| {
+            acc + if later == joltages.len() { 1 } else { arrangements_from_each[later] }
+        })
+    }
+
+    let arrangements_from_zero = joltages.iter().take_while(|&&j| j <= 3) 
+        .enumerate().fold(0, |acc, (idx, _)| acc + arrangements_from_each[idx]);
+
+    println!("[Part 2] Possible arrangements: {}", arrangements_from_zero);
+}
+
+fn main() -> Result<()> {
+    let opt = Cli::from_args();
+    let mut joltages = util::file::read_lines_to_type::<JoltageRating>(opt.file)?
+        .into_iter().map(|jr| jr.rating).collect::<Vec<u64>>();
+    
+    joltages.sort();
+
+    part1(&joltages);
+    part2(&joltages);
+    Ok(())
+}
