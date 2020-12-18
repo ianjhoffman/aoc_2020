c935b8b5b8fb35ab393ef1443e40cf1d135e47e9
Ian Hoffman <ijh6@cornell.edu>
2020-12-17 23:01:23 -0800

    Part 1

--- aoc_18/src/main.rs
+++ aoc_18/src/main.rs
@@ -4,6 +4,7 @@ use regex::Regex;
 use util::res::Result;
 use util::file::GenericParseError;
 
+#[derive(Clone, Debug, PartialEq)]
 enum Token {
     LParen,
     RParen,
@@ -12,7 +13,7 @@ enum Token {
     Num(i64),
 }
 
-fn tokenize(s: &str) -> std::result::Result<VecDeque<Token>, GenericParseError> {
+fn tokenize(s: &str) -> std::result::Result<Vec<Token>, GenericParseError> {
     lazy_static! {
         static ref TOKEN_REGEX: Regex = Regex::new(r"(\(|\)|\+|\*|[0-9]+)").unwrap();
     }
@@ -28,24 +29,86 @@ fn tokenize(s: &str) -> std::result::Result<VecDeque<Token>, GenericParseError> 
     }).collect()
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 enum Expr {
     Val(i64),
     Add(Box<Expr>, Box<Expr>),
     Mul(Box<Expr>, Box<Expr>),
 }
 
+#[derive(Clone, Debug)]
+enum Intermediate {
+    Unresolved(Token),
+    Resolved(Expr),
+}
+
 struct ExprParser {
-    tokens: VecDeque<Token>,
+    parse_stack: VecDeque<Intermediate>,
 }
 
 impl ExprParser {
-    fn new(raw: &str) -> std::result::Result<ExprParser, GenericParseError> {
-        Ok(ExprParser{ tokens: tokenize(raw)? })
+    fn new() -> ExprParser {
+        ExprParser{ parse_stack: VecDeque::new() }
     }
 
-    fn parse(&mut self) -> std::result::Result<Expr, GenericParseError> {
-        Ok(Expr::Val(0))
+    fn try_resolve_paren(&mut self) -> std::result::Result<(), GenericParseError> {
+        let prev = self.parse_stack.pop_back();
+        let prev_prev = self.parse_stack.pop_back();
+        match (prev_prev, prev) {
+            (Some(Intermediate::Unresolved(Token::LParen)), Some(Intermediate::Resolved(e))) => {
+                self.try_resolve_expr(e)
+            },
+            (pp, p) => Err(GenericParseError::ValueError(
+                format!("Unexpected closing paren succeeding [{:?}, {:?}]", pp, p).to_owned()
+            )),
+        }
+    }
+
+    fn try_resolve_expr(&mut self, e: Expr) -> std::result::Result<(), GenericParseError> {
+        let prev = self.parse_stack.pop_back();
+        let prev_prev = self.parse_stack.pop_back();
+        match (prev_prev, prev) {
+            (None, None) => {
+                self.parse_stack.push_back(Intermediate::Resolved(e));
+                Ok(()) // This is the first token
+            },
+            (pp, Some(Intermediate::Unresolved(Token::LParen))) => {
+                pp.and_then(|i| { self.parse_stack.push_back(i); Some(()) });
+                self.parse_stack.push_back(Intermediate::Unresolved(Token::LParen));
+                self.parse_stack.push_back(Intermediate::Resolved(e));
+                Ok(()) // This is the beginning of a parenthetical
+            },
+            (Some(Intermediate::Resolved(o1)), Some(Intermediate::Unresolved(Token::Add))) => {
+                self.parse_stack.push_back(Intermediate::Resolved(Expr::Add(
+                    Box::new(o1), Box::new(e)
+                )));
+                Ok(()) // This is the 2nd operand of an addition
+            },
+            (Some(Intermediate::Resolved(o1)), Some(Intermediate::Unresolved(Token::Mul))) => {
+                self.parse_stack.push_back(Intermediate::Resolved(Expr::Mul(
+                    Box::new(o1), Box::new(e)
+                )));
+                Ok(()) // This is the 2nd operand of a multiplication
+            },
+            _ => Err(GenericParseError::ValueError(format!("Unexpected expr: {:?}", e).to_owned()))
+        }
+    }
+
+    fn parse(&mut self, tokens: Vec<Token>) -> std::result::Result<Expr, GenericParseError> {
+        for token in tokens {
+            match token {
+                Token::LParen | Token::Add | Token::Mul => {
+                    self.parse_stack.push_back(Intermediate::Unresolved(token))
+                },
+                Token::RParen => { self.try_resolve_paren()?; },
+                Token::Num(v) => { self.try_resolve_expr(Expr::Val(v.clone()))?; },
+            }
+        }
+
+        match &self.parse_stack.iter().collect::<Vec<&Intermediate>>()[..] {
+            &[Intermediate::Resolved(e)] => Ok(e.clone()),
+            _ => Err(GenericParseError::ValueError("Could not resolve expression!".to_owned()))
+        }
     }
 }
 
@@ -53,7 +116,7 @@ impl std::str::FromStr for Expr {
     type Err = GenericParseError;
 
     fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
-        ExprParser::new(s)?.parse()
+        ExprParser::new().parse(tokenize(s)?)
     }
 }
 
