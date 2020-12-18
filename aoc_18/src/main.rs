7c3e8401b0edede7674bade30d6832d668ea0b3b
Ian Hoffman <ijh6@cornell.edu>
2020-12-18 13:46:18 -0800

    Less hacky precedence implementation

--- aoc_18/src/main.rs
+++ aoc_18/src/main.rs
@@ -1,15 +1,20 @@
 #[macro_use] extern crate lazy_static;
-use std::collections::VecDeque;
+use std::collections::{HashSet, VecDeque};
 use regex::Regex;
 use util::res::Result;
 use util::file::GenericParseError;
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+enum Binop {
+    Add,
+    Mul
+}
 
 #[derive(Clone, Debug, PartialEq)]
 enum Token {
     LParen,
     RParen,
-    Add,
-    Mul,
+    Binop(Binop),
     Num(i64),
 }
 
@@ -22,8 +27,8 @@ fn tokenize(s: &str) -> std::result::Result<Vec<Token>, GenericParseError> {
         Ok(match &m[1] {
             "(" => Token::LParen,
             ")" => Token::RParen,
-            "+" => Token::Add,
-            "*" => Token::Mul,
+            "+" => Token::Binop(Binop::Add),
+            "*" => Token::Binop(Binop::Mul),
             n => Token::Num(n.parse::<i64>()?),
         })
     }).collect()
@@ -32,8 +37,7 @@ fn tokenize(s: &str) -> std::result::Result<Vec<Token>, GenericParseError> {
 #[derive(Clone, Debug)]
 enum Expr {
     Val(i64),
-    Add(Box<Expr>, Box<Expr>),
-    Mul(Box<Expr>, Box<Expr>),
+    Binop(Box<Expr>, Binop, Box<Expr>),
 }
 
 #[derive(Clone, Debug)]
@@ -44,40 +48,51 @@ enum Intermediate {
 
 struct ExprParser {
     parse_stack: VecDeque<Intermediate>,
-    precedence: bool,
+    precedence_rules: Vec<HashSet<Binop>>,
 }
 
 impl ExprParser {
-    fn new(precedence: bool) -> ExprParser {
-        ExprParser{ parse_stack: VecDeque::new(), precedence }
+    fn new(precedence_rules: Vec<HashSet<Binop>>) -> ExprParser {
+        ExprParser{ parse_stack: VecDeque::new(), precedence_rules }
     }
 
-    fn try_resolve_mul(substack: &VecDeque<Intermediate>) -> std::result::Result<Expr, GenericParseError> {
-        let mut substack_clone = substack.clone();
-        while substack_clone.len() > 1 {
-            if substack_clone.len() < 3 {
-                return Err(GenericParseError::ValueError("Fewer than 3 paren-internal intermediates".to_owned()));
+    fn try_resolve_ops(&self, substack: &VecDeque<Intermediate>) -> std::result::Result<Expr, GenericParseError> {
+        let mut processing = substack.clone();
+        let mut processed: VecDeque<Intermediate> = VecDeque::new();
+        for precedence_level in &self.precedence_rules {
+            while processing.len() > 0 {
+                // Try to turn first 3 elements, which should be of the form A <binop> B, into expr C
+                let first = processing.pop_front();
+                let second = processing.pop_front();
+                let third = processing.pop_front();
+                match (first.clone(), second.clone(), third.clone()) {
+                    (Some(Intermediate::Resolved(r)), None, None) => {
+                        processed.push_back(Intermediate::Resolved(r)); // Only one item left
+                    },
+                    (
+                        Some(Intermediate::Resolved(o1)),
+                        Some(Intermediate::Unresolved(Token::Binop(op))),
+                        Some(Intermediate::Resolved(o2)),
+                    ) => {
+                        if precedence_level.contains(&op) {
+                            processing.push_front(Intermediate::Resolved(Expr::Binop(Box::new(o1), op, Box::new(o2))));
+                        } else {
+                            // Wait to apply this op since it's not in the current precedence level
+                            processed.push_back(first.unwrap());
+                            processed.push_back(second.unwrap());
+                            processing.push_front(third.unwrap()); // Keep 2nd operand to consider next
+                        }
+                    },
+                    x => return Err(GenericParseError::ValueError(
+                        format!("Could not resolve subexpression: [{:?}]", x).to_owned()
+                    )),
+                }
             }
 
-            // Try to turn first 3 elements, which should be of the form A * B, into C
-            let first = substack_clone.pop_front();
-            let second = substack_clone.pop_front();
-            let third = substack_clone.pop_front();
-            match (first, second, third) {
-                (
-                    Some(Intermediate::Resolved(o1)),
-                    Some(Intermediate::Unresolved(Token::Mul)),
-                    Some(Intermediate::Resolved(o2)),
-                ) => {
-                    substack_clone.push_front(Intermediate::Resolved(Expr::Mul(Box::new(o1), Box::new(o2))));
-                },
-                x => return Err(GenericParseError::ValueError(format!(
-                    "Could not consolidate subexpression: {:?}", x
-                ).to_owned())),
-            }
+            processing.clone_from(&processed.drain(..).collect::<VecDeque<Intermediate>>());
         }
 
-        match &substack_clone.iter().collect::<Vec<&Intermediate>>()[..] {
+        match &processing.iter().collect::<Vec<&Intermediate>>()[..] {
             &[Intermediate::Resolved(e)] => Ok(e.clone()),
             _ => Err(GenericParseError::ValueError("Could not resolve expression!".to_owned()))
         }
@@ -94,7 +109,9 @@ impl ExprParser {
 
         match in_between.pop_front() {
             Some(Intermediate::Unresolved(Token::LParen)) => {
-                self.try_resolve_expr(ExprParser::try_resolve_mul(&in_between)?)
+                Ok({ self.parse_stack.push_back(
+                    Intermediate::Resolved(self.try_resolve_ops(&in_between)?)
+                )})
             },
             f => return Err(GenericParseError::ValueError(
                 format!("Unexpected closing paren succeeding [{:?}, {:?}]", f, in_between).to_owned()
@@ -102,60 +119,19 @@ impl ExprParser {
         }
     }
 
-    fn try_resolve_expr(&mut self, e: Expr) -> std::result::Result<(), GenericParseError> {
-        let prev = self.parse_stack.pop_back();
-        let prev_prev = self.parse_stack.pop_back();
-        match (prev_prev, prev) {
-            (None, None) => {
-                self.parse_stack.push_back(Intermediate::Resolved(e));
-                Ok(()) // This is the first token
-            },
-            (pp, Some(Intermediate::Unresolved(Token::LParen))) => {
-                pp.and_then(|i| { self.parse_stack.push_back(i); Some(()) });
-                self.parse_stack.push_back(Intermediate::Unresolved(Token::LParen));
-                self.parse_stack.push_back(Intermediate::Resolved(e));
-                Ok(()) // This is the beginning of a parenthetical
-            },
-            (Some(Intermediate::Resolved(o1)), Some(Intermediate::Unresolved(Token::Add))) => {
-                self.parse_stack.push_back(Intermediate::Resolved(Expr::Add(
-                    Box::new(o1), Box::new(e)
-                )));
-                Ok(()) // This is the 2nd operand of an addition
-            },
-            (Some(Intermediate::Resolved(o1)), Some(Intermediate::Unresolved(Token::Mul))) => {
-                if self.precedence {
-                    self.parse_stack.push_back(Intermediate::Resolved(o1));
-                    self.parse_stack.push_back(Intermediate::Unresolved(Token::Mul));
-                    self.parse_stack.push_back(Intermediate::Resolved(e));
-                } else {
-                    self.parse_stack.push_back(Intermediate::Resolved(Expr::Mul(Box::new(o1), Box::new(e))));
-                }
-                Ok(()) // This is the 2nd operand of a multiplication
-            },
-            _ => Err(GenericParseError::ValueError(format!("Unexpected expr: {:?}", e).to_owned()))
-        }
-    }
-
     fn parse(&mut self, tokens: Vec<Token>) -> std::result::Result<Expr, GenericParseError> {
         for token in tokens {
             match token {
-                Token::LParen | Token::Add | Token::Mul => {
+                Token::LParen | Token::Binop(Binop::Add) | Token::Binop(Binop::Mul) => {
                     self.parse_stack.push_back(Intermediate::Unresolved(token))
                 },
                 Token::RParen => { self.try_resolve_paren()?; },
-                Token::Num(v) => { self.try_resolve_expr(Expr::Val(v.clone()))?; },
+                Token::Num(v) => {
+                    self.parse_stack.push_back(Intermediate::Resolved(Expr::Val(v)));
+                },
             }
         }
-
-        if self.precedence {
-            let mul_resolved = Intermediate::Resolved(Self::try_resolve_mul(&self.parse_stack)?);
-            self.parse_stack = vec![mul_resolved].into_iter().collect();
-        }
-
-        match &self.parse_stack.iter().collect::<Vec<&Intermediate>>()[..] {
-            &[Intermediate::Resolved(e)] => Ok(e.clone()),
-            _ => Err(GenericParseError::ValueError("Could not resolve expression!".to_owned()))
-        }
+        self.try_resolve_ops(&self.parse_stack)
     }
 }
 
@@ -163,26 +139,35 @@ impl Expr {
     fn eval(&self) -> i64 {
         match self {
             Expr::Val(v) => *v,
-            Expr::Add(e1, e2) => e1.eval() + e2.eval(),
-            Expr::Mul(e1, e2) => e1.eval() * e2.eval(),
+            Expr::Binop(e1, op, e2) => match op {
+                Binop::Add => e1.eval() + e2.eval(),
+                Binop::Mul => e1.eval() * e2.eval(),
+            },
         }
     }
 }
 
 fn part1(contents: &String) -> Result<()> {
-    let expressions = parse_expressions(&contents, false)?;
+    let expressions = parse_expressions(&contents, vec![
+        vec![Binop::Add, Binop::Mul].into_iter().collect::<HashSet<Binop>>() // +/* have equal precedence
+    ])?;
     let eval_sum = expressions.iter().fold(0, |acc, e| acc + e.eval());
     Ok(println!("[Part 1] Sum of all evaluated expressions: {}", eval_sum))
 }
 
 fn part2(contents: &String) -> Result<()> {
-    let expressions = parse_expressions(&contents, true)?;
+    let expressions = parse_expressions(&contents, vec![
+        vec![Binop::Add].into_iter().collect::<HashSet<Binop>>(), // + has highest precedence
+        vec![Binop::Mul].into_iter().collect::<HashSet<Binop>>(), // * has lower precedence
+    ])?;
     let eval_sum = expressions.iter().fold(0, |acc, e| acc + e.eval());
     Ok(println!("[Part 2] Sum of all evaluated expressions: {}", eval_sum))
 }
 
-fn parse_expressions(contents: &String, precedence: bool) -> std::result::Result<Vec<Expr>, GenericParseError> {
-    contents.lines().map(|l| ExprParser::new(precedence).parse(tokenize(l)?)).collect()
+fn parse_expressions(contents: &String, precedence_rules: Vec<HashSet<Binop>>) -> std::result::Result<Vec<Expr>, GenericParseError> {
+    contents.lines().map(|l| {
+        ExprParser::new(precedence_rules.clone()).parse(tokenize(l)?)
+    }).collect()
 }
 
 fn main() -> Result<()> {
