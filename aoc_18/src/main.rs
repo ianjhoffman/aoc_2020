3e462b1df0f7b297bf4292626de1fcb17cedc8d7
Ian Hoffman <ijh6@cornell.edu>
2020-12-17 23:59:56 -0800

    Part 2

--- aoc_18/src/main.rs
+++ aoc_18/src/main.rs
@@ -44,22 +44,60 @@ enum Intermediate {
 
 struct ExprParser {
     parse_stack: VecDeque<Intermediate>,
+    precedence: bool,
 }
 
 impl ExprParser {
-    fn new() -> ExprParser {
-        ExprParser{ parse_stack: VecDeque::new() }
+    fn new(precedence: bool) -> ExprParser {
+        ExprParser{ parse_stack: VecDeque::new(), precedence }
+    }
+
+    fn try_resolve_mul(substack: &VecDeque<Intermediate>) -> std::result::Result<Expr, GenericParseError> {
+        let mut substack_clone = substack.clone();
+        while substack_clone.len() > 1 {
+            if substack_clone.len() < 3 {
+                return Err(GenericParseError::ValueError("Fewer than 3 paren-internal intermediates".to_owned()));
+            }
+
+            // Try to turn first 3 elements, which should be of the form A * B, into C
+            let first = substack_clone.pop_front();
+            let second = substack_clone.pop_front();
+            let third = substack_clone.pop_front();
+            match (first, second, third) {
+                (
+                    Some(Intermediate::Resolved(o1)),
+                    Some(Intermediate::Unresolved(Token::Mul)),
+                    Some(Intermediate::Resolved(o2)),
+                ) => {
+                    substack_clone.push_front(Intermediate::Resolved(Expr::Mul(Box::new(o1), Box::new(o2))));
+                },
+                x => return Err(GenericParseError::ValueError(format!(
+                    "Could not consolidate subexpression: {:?}", x
+                ).to_owned())),
+            }
+        }
+
+        match &substack_clone.iter().collect::<Vec<&Intermediate>>()[..] {
+            &[Intermediate::Resolved(e)] => Ok(e.clone()),
+            _ => Err(GenericParseError::ValueError("Could not resolve expression!".to_owned()))
+        }
     }
 
     fn try_resolve_paren(&mut self) -> std::result::Result<(), GenericParseError> {
-        let prev = self.parse_stack.pop_back();
-        let prev_prev = self.parse_stack.pop_back();
-        match (prev_prev, prev) {
-            (Some(Intermediate::Unresolved(Token::LParen)), Some(Intermediate::Resolved(e))) => {
-                self.try_resolve_expr(e)
+        // Search backwards until we find an unresolved left parentheses
+        let mut in_between: VecDeque<Intermediate> = VecDeque::new();
+        while self.parse_stack.len() > 0 {
+            let prev = self.parse_stack.pop_back().unwrap();
+            in_between.push_front(prev.clone());
+            if let Intermediate::Unresolved(Token::LParen) = prev { break }
+        }
+
+        match in_between.pop_front() {
+            Some(Intermediate::Unresolved(Token::LParen)) => {
+                self.try_resolve_expr(ExprParser::try_resolve_mul(&in_between)?)
             },
-            (pp, p) => Err(GenericParseError::ValueError(
-                format!("Unexpected closing paren succeeding [{:?}, {:?}]", pp, p).to_owned()
+            f => return Err(GenericParseError::ValueError(
+                format!("Unexpected closing paren succeeding [{:?}, {:?}]", f, in_between).to_owned()
             )),
         }
     }
@@ -85,9 +123,13 @@ impl ExprParser {
                 Ok(()) // This is the 2nd operand of an addition
             },
             (Some(Intermediate::Resolved(o1)), Some(Intermediate::Unresolved(Token::Mul))) => {
-                self.parse_stack.push_back(Intermediate::Resolved(Expr::Mul(
-                    Box::new(o1), Box::new(e)
-                )));
+                if self.precedence {
+                    self.parse_stack.push_back(Intermediate::Resolved(o1));
+                    self.parse_stack.push_back(Intermediate::Unresolved(Token::Mul));
+                    self.parse_stack.push_back(Intermediate::Resolved(e));
+                } else {
+                    self.parse_stack.push_back(Intermediate::Resolved(Expr::Mul(Box::new(o1), Box::new(e))));
+                }
                 Ok(()) // This is the 2nd operand of a multiplication
             },
             _ => Err(GenericParseError::ValueError(format!("Unexpected expr: {:?}", e).to_owned()))
@@ -105,18 +147,15 @@ impl ExprParser {
             }
         }
 
+        if self.precedence {
+            let mul_resolved = Intermediate::Resolved(Self::try_resolve_mul(&self.parse_stack)?);
+            self.parse_stack = vec![mul_resolved].into_iter().collect();
+        }
+
         match &self.parse_stack.iter().collect::<Vec<&Intermediate>>()[..] {
             &[Intermediate::Resolved(e)] => Ok(e.clone()),
             _ => Err(GenericParseError::ValueError("Could not resolve expression!".to_owned()))
         }
-    }
-}
-
-impl std::str::FromStr for Expr {
-    type Err = GenericParseError;
-
-    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
-        ExprParser::new().parse(tokenize(s)?)
     }
 }
 
@@ -130,20 +169,25 @@ impl Expr {
     }
 }
 
-fn part1(expressions: &Vec<Expr>) {
+fn part1(contents: &String) -> Result<()> {
+    let expressions = parse_expressions(&contents, false)?;
     let eval_sum = expressions.iter().fold(0, |acc, e| acc + e.eval());
-    println!("[Part 1] Sum of all evaluated expressions: {}", eval_sum);
+    Ok(println!("[Part 1] Sum of all evaluated expressions: {}", eval_sum))
 }
 
-fn part2(expressions: &Vec<Expr>) {
-    
+fn part2(contents: &String) -> Result<()> {
+    let expressions = parse_expressions(&contents, true)?;
+    let eval_sum = expressions.iter().fold(0, |acc, e| acc + e.eval());
+    Ok(println!("[Part 2] Sum of all evaluated expressions: {}", eval_sum))
+}
+
+fn parse_expressions(contents: &String, precedence: bool) -> std::result::Result<Vec<Expr>, GenericParseError> {
+    contents.lines().map(|l| ExprParser::new(precedence).parse(tokenize(l)?)).collect()
 }
 
 fn main() -> Result<()> {
     let file_path = util::file::get_input_file_path();
-    let expressions = util::file::read_lines_to_type::<Expr>(file_path)?;
-
-    part1(&expressions);
-    part2(&expressions);
-    Ok(())
+    let contents = util::file::read_to_string(file_path)?;
+    part1(&contents)?;
+    part2(&contents)
 }
